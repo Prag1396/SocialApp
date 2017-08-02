9a73559e0deee5cabf9c35c32f79139b5d1bd292
Pragun Sharma <psharma5@ucsc.edu>
2017-08-02 12:47:14 -0700

    Integrating Firebase Pods

---
+++ SocialApp/SignInViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Pragun Sharma on 02/08/17.
+//  Copyright © 2017 Pragun Sharma. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+}
+
